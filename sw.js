4decbf78c4617c13ae61aa68f303c7cc76c3688e
Riccardo Bellingeri <riccardo.bellingeri@gmail.com>
2019-12-05 23:40:50 -0500

    werks

--- sw.js
+++ sw.js
@@ -2,15 +2,15 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('v1').then(function(cache) {
       return cache.addAll([
-        '/sw-test/',
-        '/sw-test/index.html',
-        '/sw-test/style.css',
-        '/sw-test/app.js',
-        '/sw-test/image-list.js',
-        '/sw-test/star-wars-logo.jpg',
-        '/sw-test/gallery/bountyHunters.jpg',
-        '/sw-test/gallery/myLittleVader.jpg',
-        '/sw-test/gallery/snowTroopers.jpg'
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/image-list.js',
+        '/star-wars-logo.jpg',
+        '/gallery/bountyHunters.jpg',
+        '/gallery/myLittleVader.jpg',
+        '/gallery/snowTroopers.jpg'
       ]);
     })
   );
@@ -18,23 +18,29 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request).then(function(response) {
+    // let event = Object.clone(event);
     // caches.match() always resolves
     // but in case of success response will have value
+    const isTarget = (_ => _ === "https://cdn-pci.optimizely.com/js/8591710953.js")(event.request.url);
+
+    if (isTarget) {
+      return fetch("./rickrolled.js")
+    }
     if (response !== undefined) {
-      return response;
+      return isTarget ? fetch("./rickrolled.js") : response;
     } else {
-      return fetch(event.request).then(function (response) {
+      return sTarget ? fetch("./rickrolled.js") : fetch(event.request).then(function (response) {
         // response may be used only once
         // we need to save clone to put one copy in cache
         // and serve second one
         let responseClone = response.clone();
         
         caches.open('v1').then(function (cache) {
-          cache.put(event.request, responseClone);
+          cache.put(event.request, responseClone).catch((e) => console.warn(e));
         });
         return response;
       }).catch(function () {
-        return caches.match('/sw-test/gallery/myLittleVader.jpg');
+        return caches.match('/gallery/myLittleVader.jpg');
       });
     }
   }));
